1a13fab0eb1dda1bb1297325b9c866932b7a5fb7
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2019-12-17 02:22:03 +0900

    Update Main.java

--- ch20/hw/Main.java
+++ ch20/hw/Main.java
@@ -2,7 +2,7 @@ package ch20.hw;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("20161142 조희준");
-        BigCharGui bigCharGui = new BigCharGui("20161142 조희준");
+        System.out.println("");
+        BigCharGui bigCharGui = new BigCharGui("");
     }
 }
